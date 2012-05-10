8a832d045f33f837a17745f5694f7828f7abdb2f
Alex J. Champandard <alexjc@aigamedev.com>
2012-05-11 00:49:11 +0200

    Script for running the final competition and gathering the statistics.

--- competition.py
+++ competition.py
@@ -3,24 +3,40 @@ import random
 from resistance import Game
 from stock import RandomPlayer, SimplePlayer
 
+from daniele import IAmNotASpyPlayer
+from myplayers import Simpletonian
+from mvbStock import Rogue
+from jekyll import JekyllPlayer
+from SoreLooser import SoreLooser
+from trusty import Trusty
 
-competitors = [RandomPlayer, SimplePlayer]
+competitors = [SimplePlayer, IAmNotASpyPlayer, Simpletonian, JekyllPlayer, SoreLooser, Trusty, Rogue] # RandomPlayer
 statistics = {}
 
 
 class Statistic:
     def __init__(self):
-        self.resistance = 0
-        self.spies = 0
+        self._resistance = 0
+        self._spies = 0
         self.plays = 0
 
-    def __repr__(self):
-        return "(RESISTANCE %0.1f%%, SPY %0.1f%%)" % (float(self.resistance) * 100.0 / float(self.plays), float(self.spies) * 100.0 / float(self.plays))
+    def total(self):
+        return float(self._resistance + self._spies) / float(self.plays)
 
-GAMES = 10000
+    def spies(self):
+        return float(self._spies) / float(self.plays)
+
+    def resistance(self):
+        return float(self._resistance) / float(self.plays)
+
+
+GAMES = 100000
 
 for i in range(0,GAMES):
-    players = [random.choice(competitors) for x in range(0,5)]
+    if i % 10000 == 0: print '.'
+
+    # players = [random.choice(competitors) for x in range(0,5)]
+    players = random.sample(competitors, 5)
     g = Game(players)
     g.run()
 
@@ -32,11 +48,23 @@ for i in range(0,GAMES):
 
         if p.spy and not win:
             s = statistics.get(p.name, Statistic())
-            s.spies += 1
+            s._spies += 1
             statistics[p.name] = s
         if not p.spy and win:
             s = statistics.get(p.name, Statistic())
-            s.resistance += 1
+            s._resistance += 1
             statistics[p.name] = s
 
-print statistics
+
+print "\nSPIES" 
+for s in sorted(statistics.items(), key = lambda x: -x[1].spies()):
+    print " ", s[0], "\t", s[1].spies() * 100.0
+
+print "\nRESISTANCE" 
+for s in sorted(statistics.items(), key = lambda x: -x[1].resistance()):
+    print " ", s[0], "\t", s[1].resistance() * 100.0
+
+print "\nTOTAL" 
+for s in sorted(statistics.items(), key = lambda x: -x[1].total()):
+    print " ", s[0], "\t", s[1].total() * 100.0
+
