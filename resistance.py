63085dadbace4ee06f8f343f5feca56a66c9b799
Alex J. Champandard <alexjc@aigamedev.com>
2012-05-11 13:45:56 +0200

    Added parameterizable number of turns, wins and losses.

--- resistance.py
+++ resistance.py
@@ -19,7 +19,11 @@ class State:
 
 
 class Game:
-    
+
+    NUM_TURNS = 5
+    NUM_WINS = 3
+    NUM_LOSSES = 3
+   
     def __init__(self, players):
         roles = [True, True, False, False, False]
         random.shuffle(roles)
@@ -28,7 +32,7 @@ class Game:
         
         self.states = [State(p.index, p.name) for p in self.players]
     
-        self.participants = [2, 3, 2, 3, 3]
+        self.participants = [2, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 
         self.wins = 0
         self.leader = 0
@@ -40,11 +44,11 @@ class Game:
         spies = [self.states[p.index] for p in self.players if p.spy]
         for p in self.players:
             if p.spy:   
-                p.reveal(self.states, spies)
+                p.reveal(self.states[:], spies[:])
             else:
-                p.reveal(self.states, [])
+                p.reveal(self.states[:], [])
 
-        while self.turn < 5:
+        while self.turn < self.NUM_TURNS:
             if self.step():
                 self.turn += 1
                 self.tries = 0
@@ -55,13 +59,13 @@ class Game:
                 self.wins = 0
                 break
 
-            if self.wins >= 3:
+            if self.wins >= self.NUM_WINS:
                 break
-            if self.turn - self.wins >= 3:
+            if self.turn - self.wins >= self.NUM_LOSSES:
                 break
         
         for p in self.players:
-            p.onGameComplete(self.states, spies)
+            p.onGameComplete(self.states[:], spies)
 
     def step(self):
         l = self.players[self.leader]
@@ -70,7 +74,7 @@ class Game:
             self.leader = 0 
 
         count = self.participants[self.turn]
-        selected = l.select(self.states, count)
+        selected = l.select(self.states[:], count)
         assert type(selected) is list, "Expecting a list as a return value of select()."
         assert len(set(selected)) == count, "The list returned by select() is of the wrong size!"
         for s in selected: assert isinstance(s, State), "Please return objects from the list passed to select()."
@@ -78,27 +82,30 @@ class Game:
         votes = []
         score = 0
         for p in self.players:
-            v = p.vote(selected, self.states[l.index], self.tries)
+            v = p.vote(selected[:], self.states[l.index], self.tries)
             assert type(v) is bool, "Please return a boolean from vote()."
             votes.append(v)
             if v:
                 score += 1
 
         for p in self.players:
-            p.onVoteComplete(self.states, votes, selected)
+            p.onVoteComplete(self.states[:], votes, selected[:])
 
         if score <= 2:
             return False 
 
         sabotaged = 0
         for s in selected:
-            sabotaged += int(self.players[s.index].sabotage(selected))
+            p = self.players[s.index]
+            result = p.sabotage(selected[:])
+            if not p.spy: assert result == False, "The function sabotage() cannot return True for Resistance fighters."
+            sabotaged += int(result)
 
         if sabotaged == 0:
             self.wins += 1
             
         for p in self.players:
-            p.onMissionComplete(selected, sabotaged)
+            p.onMissionComplete(selected[:], sabotaged)
 
         return True
 
